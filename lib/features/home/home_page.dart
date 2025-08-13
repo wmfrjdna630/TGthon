00520bbccce63c3e2a8ee97382261adfe45ca544
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2025-08-14 00:06:45 +0900

    연습

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -39,7 +39,7 @@ class HomePage extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
                       Text(
-                        'Hello! 👋',
+                        'ddd! 👋',
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.bold,
