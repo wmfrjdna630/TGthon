9bf3a26d0c63107691d39550ad5b7535a8d52e3b
wmfrjdna630 <111439483+wmfrjdna630@users.noreply.github.com>
2025-08-13 23:29:21 +0900

    몰라 일단 이거 됨 안되면 jdk21다운받아

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,9 @@ plugins {
 android {
     namespace = "com.example.flutter_work"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // ✅ NDK 버전 강제 (플러그인 요구 버전)
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -23,11 +25,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_work"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+
+        // ✅ minSdk 올리기 (firebase_auth가 23 요구)
+        minSdk = 23
+
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -35,8 +37,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
